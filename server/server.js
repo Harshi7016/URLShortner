a8da9df9428eeaf93c8649eb1a74ab741b02bcf6
Harshith7016 <harshithece7016@gmail.com>
2021-01-07 18:24:04 +0530

    feat: URL Shortner  EJS

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const ShortUrl = require('./model/shortUrl');
+const app = express();
+const mongoDbUrl = process.env.CONNECTION_URL;
+console.log({ mongoDbUrl });
+mongoose
+  .connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected…');
+  })
+  .catch((err) => console.log(err));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl === null) return res.sendStatus(404);
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 8080);
